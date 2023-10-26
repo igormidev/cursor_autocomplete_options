358d691214d35ea32b9224bde29d61afc88c642a
Igor <igor_midev@outlook.com>
2023-10-25 22:07:15 -0300

    GH-PAGES

--- CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-## [1.1.0] 25-09-2023
-* Web scroll fix.
-* Border added to first and last item tiles 
-
-## [1.0.0] 25-09-2023
-* First release.
--- lib/cursor_autocomplete_options.dart
+++
@@ -1,5 +0,0 @@
-library cursor_autocomplete_options;
-
-export 'src/options_controller.dart' show OptionsController;
-export 'src/overlay_choices_listview_widget.dart'
-    show OverlayChoicesListViewWidget;
